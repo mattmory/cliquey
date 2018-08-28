cb702b9e29da7f20911ae757a4ac1ce91aab4d85
mattmory <matt.mory@gmail.com>
2018-08-27 22:27:09 -0700

    update

--- src/components/Crusty/Crusty.js
+++ src/components/Crusty/Crusty.js
@@ -5,7 +5,7 @@ const Crusty = props => (
     <div className="crusty">
     <div className="row">
         <div className="col-md-6">
-        <img src="./Images/crusty.jpg" alt="Crusty"/>
+        <img src="./Images/Crusty.jpg" alt="Crusty"/>
         </div>
         <div className="col-md-6 instructions">
         <span className="instText">Hey Hey kids!!! Pick each character once.. get to 12 and win a prize!</span>
